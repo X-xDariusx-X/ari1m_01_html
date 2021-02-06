8ff7c72f4f2979b62ced4e7aefa8d5aeb30191b4
X-xDariusx-X <dariusnic1rgmail.com>
2021-02-06 13:27:31 +0100

    Trabajo

---
+++ Ejercicios InIn/7º trabajo/Arrays.php
@@ -0,0 +1,25 @@
+<?php
+$nombres =[
+	'Andrei',
+	'Fabián',
+	'Héctor',
+	'Gabi',
+	'Nury',
+	'Agustina',
+	'Matheu',
+	'Dani',
+	'Ismael',
+	'Pepe',
+	'Javier',
+	'Rodras',
+	'Tregallo',
+	'Alfonso',
+	'Darius',
+	'Ivan',
+	'Tejería',
+];
+//Ordenados alfabeticamente
+echo "Quinto lugar en orden alfabetico <br>";
+sort($nombres);
+var_export($nombres)[5-1];
+?>
